027c9d3471d112dae563584c5f3f1133d4642619
EmilEbulisov <146378096+EmilEbulisov@users.noreply.github.com>
2024-01-24 18:13:03 +0300

    Update MySubstr.js

--- MySubstr.js
+++ MySubstr.js
@@ -1,12 +1,17 @@
 const mySubstr = (text, long) => {
       let i = 0;
       result = '';
+    if (long < text.length){
       
-      while(i<long){
+        while(i<long){
         result = `${result}${text[i]}`;
-        
         i++;
-      }
+        }
       console.log(`${result}` );
+    }
+    else{
+      console.log('noup');
+    }
+  
 }
-mySubstr('I dont know this materials', 6);
+mySubstr('I dont know this materials', 5);
